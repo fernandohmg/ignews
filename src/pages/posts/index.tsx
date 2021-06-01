c88044d8ca66accf898693154993dd43b29283f7
Fernando Goncalves <fernando.hmg@gmail.com>
2021-06-01 00:04:22 -0300

    using prismic api, routes of application

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,21 @@
+import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
-export default function Posts() {
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -8,44 +23,49 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Dark Mode com CSS — mudando a aparência do Blog de maneira simples
-              e rápida
-            </strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Dark Mode com CSS — mudando a aparência do Blog de maneira simples
-              e rápida
-            </strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>
-              Dark Mode com CSS — mudando a aparência do Blog de maneira simples
-              e rápida
-            </strong>
-            <p>
-              Umas das funcionalidades que está na moda em Blogs e Sites é o
-              Dark Mode. Devs, em sua maioria, curtem bastante utilizar temas
-              escuros, tanto na IDE quanto em outros apps.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
